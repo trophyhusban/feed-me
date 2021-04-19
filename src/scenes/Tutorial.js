70a682dd027d7a56592333dc4feeb3c75d641a41
Ardent Reinhard <ardent@ucsc.edu>
2021-04-18 17:19:25 -0700

    polished the tutorial screen & the colorscheme

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -9,7 +9,7 @@ class Tutorial extends Phaser.Scene {
         this.load.spritesheet("red mouth", "./assets/red_mouth.png", {frameWidth:120, frameHeight:64, startFrame:0, endFrame: 6});
         this.load.spritesheet("eyeball", "./assets/eyeball.png", {frameWidth: 32, frameHeight: 32, startFrame:0, endFrame: 0});
         this.load.spritesheet("eye", "./assets/eye_blink.png", {frameWidth: 120, frameHeight: 64, startFrame:0, endFrame:5} );
-        this.load.image("tutorial", "./assets/tutorial.png", {frameWidth: 640, frameHeight: 480});
+        this.load.image("tutorial text", "./assets/tutorial_text.png");
     }
 
     create() {
@@ -122,7 +122,7 @@ class Tutorial extends Phaser.Scene {
             game.config.height, 
             0xFFFFFF
             ).setOrigin(0 ,0);
-	    this.add.rectangle(
+	    this.frame = this.add.rectangle(
             game.config.width - borderUISize, 
             0, 
             borderUISize, 
@@ -134,7 +134,19 @@ class Tutorial extends Phaser.Scene {
         this.blinkTimer = 0;
         this.nextBlink = Math.floor(Math.random()*500) + 1000;
 
-        this.add.sprite(0, 0, "tutorial").setOrigin(0, 0);
+        this.textbox = this.add.sprite(borderUISize*1.5, config.height-128-borderUISize*1.5, "tutorial text").setOrigin(0, 0);
+        
+        this.textTriangle = [];
+        this.updateTriangle(this.textTriangle);
+
+        this.tri = this.add.polygon(
+            0,
+            0,
+            this.textTriangle,
+            0xFFFFFF
+            ).setOrigin(0, 0);
+        console.log(this.tri);
+        console.log(this.frame);
 
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
     }
@@ -156,11 +168,57 @@ class Tutorial extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyF)) {
             this.scene.start("menuScene");
         }
+        this.tri.destroy();
+        this.updateTriangle(this.textTriangle);
+        this.tri = this.add.polygon(
+            0,
+            0,
+            this.textTriangle,
+            0xFFFFFF
+            ).setOrigin(0, 0);
     }
+
     animateTiles() {
         this.checkers.tilePositionX -= .5;
         this.checkers.tilePositionY -= .5;
         this.grid.tilePositionX += .25;
         this.grid.tilePositionY += .25;
+    }
+    updateTriangle(points) {
+        let maxWidth = 128 + 32;
+        let currentWidth = 0;
+        let bottomWidth = 48;
+        let x1, y1, x2, y2, x3, y3;
+        if (points.length == 0) {
+            x1 = this.mouth2.x + this.mouth2.width/2;
+            y1 = this.mouth2.y + this.mouth2.height/2;
+            x2 = this.textbox.x + currentWidth;
+            y2 = this.textbox.y+1;
+            x3 = this.textbox.x + bottomWidth;
+            y3 = this.textbox.y+1;
+        } else {
+            x1 = this.mouth2.x + this.mouth2.width/2;
+            y1 = points[1];
+            x2 = points[2];
+            y2 = points[3];
+            x3 = points[4];
+            y3 = points[5];
+        }
+
+        x2 = x1 - bottomWidth/2;
+        x3 = x1 + bottomWidth/2;
+
+        if (x2 < this.textbox.x + maxWidth) {
+            x2 = this.textbox.x + maxWidth;
+        }
+
+        if (x2 > this.textbox.x + this.textbox.width - maxWidth - bottomWidth) {
+            x2 = this.textbox.x + this.textbox.width - maxWidth - bottomWidth;
+        }
+
+        x3 = x2 + bottomWidth;
+
+        this.textTriangle = [x1, y1, x2, y2, x3, y3];
+
     }
 }
